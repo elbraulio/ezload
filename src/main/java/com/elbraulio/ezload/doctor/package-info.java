f6da096a1e71c7ff2bd3b89b2605da4adb036bbc
elbraulio <elbraulio274@gmail.com>
2020-09-05 12:07:02 -0400

    #54 includes package-info.java and license

--- src/main/java/com/elbraulio/ezload/doctor/package-info.java
+++ src/main/java/com/elbraulio/ezload/doctor/package-info.java
@@ -22,21 +22,7 @@
  * SOFTWARE.
  */
 
-package com.elbraulio.ezload.error;
-
-import java.util.List;
-
-public class RowError implements EzError {
-    private final int row;
-    private final List<String> errors;
-
-    public RowError(int row, List<String> errors) {
-        this.row = row;
-        this.errors = errors;
-    }
-
-    @Override
-    public String toString() {
-        return "error at row " + this.row + ": " + this.errors;
-    }
-}
+/**
+ * This packages contains implementations of Doctor.
+ */
+package com.elbraulio.ezload.doctor;
